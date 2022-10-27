be3b09ed65d443e931cf9744e5a6c84e19f9c841
Calvin <1176479962@qq.com>
2022-10-27 11:53:58 +0800

    inplement graph

--- build_graph.py
+++ build_graph.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import pickle as pkl
+import easygraph as eg
 import os
 
 file_names = {
@@ -42,14 +44,88 @@ class DataLoader:
 
     def load_music_id_list(self):
         file_path = self.file_paths["music_id_list"]
-        return np.loadtxt(file_path, dtype=np.int64)
+        return np.loadtxt(file_path, dtype=object)
     
     def load_music_data(self):
         file_path = self.file_paths["music_data"]
-        dtypes = {"id": np.int64}
+        dtypes = {"id": object}
         return pd.read_csv(file_path, index_col="num", dtype=dtypes)
 
     def load_music_comments(self):
         file_path = self.file_paths["music_comments"]
-        dtypes = {"music_id": np.int64, "user_id": np.int64, "timestamp": np.int64, "comment_id": np.int64, "liked_count": np.int64}
+        dtypes = {"music_id": object, "user_id": np.int64, "timestamp": np.int64, "comment_id": np.int64, "liked_count": np.int64}
         return pd.read_csv(file_path, dtype=dtypes)
+
+    def load_graph(self, use_file=True, save_file=True, file_name="graph.pkl"):
+        if use_file:
+            try:
+                print(f"try to load graph from file {file_name} ...")
+                with open(file_name, "rb") as f:
+                    graph = pkl.load(f)
+                return graph
+            except:
+                print(f"fail to load {file_name}\nbuild now ...")
+        
+        graph = eg.DiGraph()
+
+        #user_id_list
+        user_id_list = self.load_user_id_list()
+        for user_id in user_id_list:
+            graph.add_node(user_id, type="user")
+        del user_id_list
+        
+        #userInfo
+        # userInfo = self.load_userInfo()
+        # for index, row in userInfo.iterrows():
+        #     id = row["id"]
+        #     graph.nodes[id]["name"] = row["name"]
+        #     graph.nodes[id]["description"] = row["description"]
+        # del userInfo
+
+        #user_follow
+        user_follow = self.load_user_follow()
+        for index, row in user_follow.iterrows():
+            user_id, follow_id = row["user_id"], row["follow_id"]
+            assert(user_id in graph and follow_id in graph)
+            graph.add_edge(user_id, follow_id, type="follow")
+        del user_follow
+
+        #user_followed
+        user_followed = self.load_user_followed()
+        for index, row in user_followed.iterrows():
+            user_id, followed_id = row["user_id"], row["followed_id"]
+            assert(user_id in graph and followed_id in graph)
+            graph.add_edge(followed_id, user_id, type="follow")
+        del user_followed
+
+        #music_id_list
+        music_id_list = self.load_music_id_list()
+        for music_id in music_id_list:
+            graph.add_node(music_id, type="music")
+        del music_id_list
+
+        #music_data
+        music_data = self.load_music_data()
+        for index, row in music_data.iterrows():
+            id = row["id"]
+            graph.nodes[id]["singer"] = row["singer"]
+            graph.nodes[id]["album"] = row["album"]
+            graph.nodes[id]["comment_num"] = row["comment_num"]
+            graph.nodes[id]["play_list"] = row["playlist"]
+        del music_data
+
+        music_comments = self.load_music_comments()
+        for index, row in music_comments.iterrows():
+            graph.add_edge(row["user_id"], row["music_id"], timestamp=row["timestamp"], liked_count=row["liked_count"])
+        del music_comments
+
+        if save_file:
+            with open(file_name, "wb") as f:
+                pkl.dump(graph, f)
+
+        return graph
+        
+
+if __name__ == "__main__":
+    loader = DataLoader()
+    graph = loader.load_graph()
