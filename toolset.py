709b491ccd222d698bd0aa05b22d95429d5fb444
Calvin <1176479962@qq.com>
2023-01-02 19:15:04 +0800

    metrics

--- toolset.py
+++ toolset.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 
 def class_transform(G, class_=nx.Graph):
@@ -16,3 +17,12 @@ def gen_test_embedding(graph, length=5):
     data = pd.DataFrame(data, columns = ["id"] + ["emb_" + str(i) for i in range(length)])
     data.set_index(["id"], inplace=True)
     return data
+
+def metrics(y, y_pred, verbose=True):
+    accuracy, precision, recall, f1 = accuracy_score(y, y_pred), precision_score(y, y_pred), recall_score(y, y_pred), f1_score(y, y_pred)
+    if verbose:
+        print("Accuracy: %.2f%%"%(accuracy * 100.0))
+        print('Precision: %.2f%%'%(precision * 100.0))
+        print('Recall: %.2f%%'%(recall * 100.0))
+        print('F1-score: %.2f%%'%(f1 * 100.0))
+    return accuracy, precision, recall, f1
